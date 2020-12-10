f85edcf5cd7e6e498a00441852d1e009af284970
Jackson Tarlin <jacksontarlin@gmail.com>
2020-12-10 15:47:57 -0500

    niced up the image selection functions for character creator

--- Components/ImageDisplay.js
+++ Components/ImageDisplay.js
@@ -5,7 +5,7 @@ import SubraceImages from "./SubraceImages";
 
 // const drawnPath = "../assets/images/drawn/";
 
-export default function ImageDisplay({subrace}) {
+export default function ImageDisplay({subrace, setImage, toggleModal}) {
 
     let imagePaths;
 
@@ -28,7 +28,10 @@ export default function ImageDisplay({subrace}) {
 
         <View style={{flex: 1, flexDirection: "row", flexWrap: "wrap", justifyContent: "space-around", marginTop: 30}}>
             {imagePaths.map(image=>{
-                return <Image style={{height: 100, width: 100, borderRadius: 50, borderWidth: 3, borderColor: "black"}} key={image.id} source={image.image} />
+                return (
+                <TouchableOpacity onPress={()=>{setImage(image.image); toggleModal();}}>
+                    <Image style={{height: 100, width: 100, borderRadius: 50, borderWidth: 3, borderColor: "black"}} key={image.id} source={image.image} />
+                </TouchableOpacity>)
             })}
         </View>
     )
--- Screens/CreateCharacter/CreateCharacter.js
+++ Screens/CreateCharacter/CreateCharacter.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text, StyleSheet, Modal, TouchableHighlight, TouchableOpacity} from 'react-native';
+import { View, Text, StyleSheet, Modal, TouchableHighlight, TouchableOpacity, Image} from 'react-native';
 import CustomHeader from "../../Components/CustomHeader";
 import {Picker} from '@react-native-picker/picker';
 // import { TouchableOpacity } from 'react-native-gesture-handler';
@@ -11,7 +11,7 @@ function CreateCharacter(props) {
     const navigation = props.navigation;
 
     //keeps track of our important character info, written to database when submit is pressed
-    const [characterObj, setCharacterObj] = React.useState({race: "", subrace: ""});
+    const [characterObj, setCharacterObj] = React.useState({race: null, subrace: null, image: null});
 
     //tracks whether the appearance modal is open or not
     const [modalVisible, setModalVisible] = React.useState(false);
@@ -23,6 +23,7 @@ function CreateCharacter(props) {
                 selectedValue={characterObj.subrace}
                 style={styles.selector}
                 onValueChange={(itemValue) => {setCharacterObj({...characterObj, subrace: itemValue})}}>
+                <Picker.Item label="Choose Subrace" value={null} />
                 <Picker.Item label="Tomorran" value="tomorran" />
                 <Picker.Item label="Cyborg" value="cyborg" />
                 </Picker>
@@ -32,6 +33,7 @@ function CreateCharacter(props) {
                 selectedValue={characterObj.subrace}
                 style={styles.selector}
                 onValueChange={(itemValue) => {setCharacterObj({...characterObj, subrace: itemValue})}}>
+                <Picker.Item label="Choose Subrace" value={null} />
                 <Picker.Item label="Android" value="android" />
                 <Picker.Item label="Husk" value="husk" />
                 </Picker>
@@ -41,14 +43,22 @@ function CreateCharacter(props) {
                 selectedValue={characterObj.subrace}
                 style={styles.selector}
                 onValueChange={(itemValue) => {setCharacterObj({...characterObj, subrace: itemValue})}}>
-                <Picker.Item label="Select Species First!" value="" />
+                <Picker.Item label="Select Race First!" value="" />
                 </Picker>
                 break
         }
     }
 
     function toggleModal() {
-        setModalVisible(!modalVisible);
+        if(characterObj.race && characterObj.subrace) {
+            setModalVisible(!modalVisible);
+        }
+        
+    }
+
+    function setCharacterImage(img) {
+        console.log("running set character image function");
+        setCharacterObj({...characterObj, image: img});
     }
 
     return (
@@ -76,24 +86,25 @@ function CreateCharacter(props) {
                         <Text >Hide Modal</Text>
                     </TouchableHighlight>
 
-                    <ImageDisplay subrace={characterObj.subrace} />
+                    <ImageDisplay subrace={characterObj.subrace} setImage={setCharacterImage} toggleModal={toggleModal} />
                 </View>
             </Modal>
 
             <View style={{flex: 1, alignItems: "center"}}>
-                <Text>Choose Species</Text>
+                <Text>Choose Race</Text>
                 <Picker
                     selectedValue={characterObj.race}
                     style={styles.selector}
                     onValueChange={(itemValue) => {setCharacterObj({...characterObj, race: itemValue})}}>
+                    <Picker.Item label="Choose Race" value={null} />
                     <Picker.Item label="Human" value="human" />
                     <Picker.Item label="Robot" value="robot" />
                 </Picker>
-                <Text>Choose Subspecies</Text>
+                <Text>Choose Subrace</Text>
                 {setSubrace(characterObj.race)}
                 <Text>Choose Appearance</Text>
                 <TouchableOpacity onPress={toggleModal}>
-                    <View style={{borderRadius: 50, border: "3px solid black", height: 100, width: 100, backgroundColor: "gray"}}></View>
+                    <Image style={{height: 100, width: 100, borderRadius: 50, borderWidth: 3, borderColor: "black"}} source={characterObj.image} />
                 </TouchableOpacity>
 
             </View>
