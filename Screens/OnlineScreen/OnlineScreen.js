aeb36b265ca60322995bbcb6421b86b4f3b52eb9
Jackson Tarlin <jacksontarlin@gmail.com>
2020-12-14 12:41:46 -0500

    added create and join buttons to online screen

--- Screens/OnlineScreen/OnlineScreen.js
+++ Screens/OnlineScreen/OnlineScreen.js
@@ -11,7 +11,16 @@ export default function OnlineScreen({navigation}){
     return (
         <View style={{flex:1}}>
             <CustomHeader title={"O N L I N E"} goBack={()=>{navigation.goBack()}}/>
-
+            <TouchableOpacity onPress={()=>{console.log("pressed crete nrw game")}}>
+                <View style={{height: 60, width: "100%", backgroundColor: "#98b8eb"}}>
+                    <Text>CREATE NEW GAME</Text>
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={()=>{console.log("pressed join nrw game")}}>
+                <View style={{height: 60, width: "100%", backgroundColor: "#98b8eb"}}>
+                    <Text>JOIN NEW GAME</Text>
+                </View>
+            </TouchableOpacity>
         </View>
     )
 }
